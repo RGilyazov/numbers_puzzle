bfbfa77f086f7f6caef8632c4656364cb85b9134
bbOracul <GilyazovRuslan@Gmail.com>
2022-08-05 14:54:06 +0500

    initial

---
+++ src/app/App.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+// import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+
+function App() {
+  // const dispatch = useDispatch();
+  // const { isLoading, hasError } = useSelector((state) => state.allRecipes);
+
+  // useEffect(() => {
+  //   dispatch(loadRecipes());
+  // }, [dispatch]);
+
+  // const onTryAgainHandler = () => {
+  //   dispatch(loadRecipes());
+  // };
+
+  return (
+      <main>
+        <h1>To do...</h1>
+      </main>
+    );
+}
+
+export default App;
+
---
+++ src/app/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+//import allRecipesReducer from "../features/allRecipes/allRecipesSlice";
+
+export default configureStore({
+  reducer: {
+  //  allRecipes: allRecipesReducer,
+  //  favoriteRecipes: favoriteRecipesReducer,
+  //  search: searchReducer,
+  },
+});
