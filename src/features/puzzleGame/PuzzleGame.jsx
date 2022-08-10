7d1aac7d546eed3d99c303dd87dc08157a8034f5
bbOracul <GilyazovRuslan@Gmail.com>
2022-08-10 14:20:40 +0500

    prevent page reload

--- src/features/puzzleGame/PuzzleGame.jsx
+++ src/features/puzzleGame/PuzzleGame.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import Glass from "./Glass";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 import Menu from "../about/Menu";
 import Layout from "../about/Layout";
 
 export default function PuzzleGame() {
   const state = useSelector((state) => state.puzzleGame);
+
+  useEffect(() => {
+    const onUnload = (e) => {
+      e.preventDefault();
+      e.returnValue = "game progress will be lost";
+      return e.returnValue;
+    };
+    window.addEventListener("beforeunload", onUnload);
+    return () => {
+      window.removeEventListener("beforeunload", onUnload);
+    };
+  });
   const { rows, topRow } = state;
   return (
     <Layout
@@ -15,5 +28,4 @@ export default function PuzzleGame() {
       ]}
     ></Layout>
   );
-
 }
